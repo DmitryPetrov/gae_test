ae07102118fd0a406e970777ec13721834deb4f5
Dmitry Petrov <dmitrypetrov93@gmail.com>
2019-02-03 23:22:25 +0300

    First commit

---
+++ src/main/java/gae/HelloFromGae.java
@@ -0,0 +1,55 @@
+
+package gae;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloFromGae
+ */
+@WebServlet(name = "HelloAppEngine", value = "/greeting")
+public class HelloFromGae extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor.
+     */
+    public HelloFromGae() {
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+     *      response)
+     */
+    protected void doGet(HttpServletRequest request,
+            HttpServletResponse response) throws ServletException, IOException {
+
+        String name = (String) request.getParameter("name");
+
+        if (name == null) {
+            response.getWriter().append("Served at: ")
+                    .append(request.getContextPath());
+            ;
+        } else {
+            response.getWriter().append(
+                    "<h1> Hello from Google App Engine, " + name + "</h1>")
+                    .close();
+        }
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+     *      response)
+     */
+    protected void doPost(HttpServletRequest request,
+            HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        doGet(request, response);
+    }
+
+}
